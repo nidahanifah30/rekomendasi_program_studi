0c72d38d2c6a4f807d56aa6a348638722b2a76c9
Nida Hanifah <nidahanifah412my@gmail.com>
2025-06-17 23:26:03 +0700

    Add files via upload

--- config/db.go
+++ config/db.go
@@ -1,1 +1,20 @@
+package config
 
+import (
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDB() {
+	dsn := "root:@tcp(127.0.0.1:3306)/rekomendasi_programstudi?charset=utf8mb4&parseTime=True&loc=Local"
+	var err error
+
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Gagal koneksi ke database:", err)
+	}
+}
